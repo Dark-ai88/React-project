949128b53fc0a7f4d0cfe0b0e1d593a76f9f451a
Alexander <Alexander@WIN-KBP1QF0NNMR>
2024-01-28 20:25:37 +0300

    react-app/public/favicon.ico

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavBar = () => {
+    return (
+        <nav className='app-nav'>
+            navbar
+        </nav>
+    )
+};
+
+export default NavBar;
---
+++ src/components/ProfilePage/ProfilePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProfilePage = () => {
+    return (
+        <div className='app-content'>
+            main content
+        </div>
+    )
+};
+
+export default ProfilePage;
