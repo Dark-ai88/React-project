7b1074917c471272f7a47f57d315fd07386536ed
Alexander <Alexander@WIN-KBP1QF0NNMR>
2024-02-01 21:32:37 +0300

    react-app/src/components/MessagePage/MessagePage.jsx

---
+++ src/components/MessagePage/MessagePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import s from './MessagePage.module.css';
+
+const MessagePage = () => {
+    return (
+        <div className={s.app_message}>
+            MessagePage
+        </div>
+    )
+};
+
+export default MessagePage;
