c62ae2f9d84acf2dad96f0c034e3e1f451326f1e
Alexander <Alexander@WIN-KBP1QF0NNMR>
2024-02-02 13:40:39 +0300

    react-app/src/components/NavBar/NavBar.jsx

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,23 +7,23 @@ const NavBar = () => {
         <nav className={s.app_nav}>
             <div className={s.app_item_link}>
                 <img src="https://cdn-icons-png.flaticon.com/128/6063/6063734.png" alt="" />
-                <NavLink to="/profile">profile</NavLink>
+                <NavLink to="/profile" className={navData => navData.isActive ? s.active : s.item}>profile</NavLink>
             </div>
             <div className={s.app_item_link}>
                 <img src="https://cdn-icons-png.flaticon.com/128/6939/6939143.png" alt="" />
-                <NavLink to="/message">message</NavLink>
+                <NavLink to="/message" className={navData => navData.isActive ? s.active : s.item}>message</NavLink>
             </div>
             <div className={s.app_item_link}>
                 <img src="https://cdn-icons-png.flaticon.com/128/9511/9511346.png" alt="" />
-                <NavLink to="/news">news</NavLink>
+                <NavLink to="/news" className={navData => navData.isActive ? s.active : s.item}>news</NavLink>
             </div>
             <div className={s.app_item_link}>
                 <img src="https://cdn-icons-png.flaticon.com/128/3233/3233695.png" alt="" />
-                <NavLink to="/music">music</NavLink>
+                <NavLink to="/music" className={navData => navData.isActive ? s.active : s.item}>music</NavLink>
             </div>
             <div className={s.app_item_link}>
                 <img src="https://cdn-icons-png.flaticon.com/128/4043/4043261.png" alt="" />
-                <NavLink to="/settings">settings</NavLink>
+                <NavLink to="/settings" className={navData => navData.isActive ? s.active : s.item}>settings</NavLink>
             </div>
         </nav>
     )
