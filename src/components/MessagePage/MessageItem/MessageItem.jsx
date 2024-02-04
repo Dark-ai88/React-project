f95343526ba9cef71f3205ad717584ae7f66fbe1
Alexander <Alexander@WIN-KBP1QF0NNMR>
2024-02-04 17:57:57 +0300

    react-app/src/components/MessagePage/MessageItem/MessageItem.jsx

--- src/components/MessagePage/MessageItem/MessageItem.jsx
+++ src/components/MessagePage/MessageItem/MessageItem.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import s from './../MessagePage.module.css';
 
-
 const MessageItem = (props) => {
     return props.state.messageItem.map((item) =>
         <div className={s.app_item}>{item.message}</div>
--- src/components/MessagePage/UsersItem/UsersItem.jsx
+++ src/components/MessagePage/UsersItem/UsersItem.jsx
@@ -11,6 +11,4 @@ const UsersItem = (props) => {
     )
 };
 
-
-
 export default UsersItem;
