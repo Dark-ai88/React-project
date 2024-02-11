565ecde74eef14103d29eb0f7b057b41f7d12ef6
Alexander <Alexander@WIN-KBP1QF0NNMR>
2024-02-11 19:20:35 +0300

    react-app/src/components/MessagePage/MessagePage.module.css

--- src/components/MessagePage/UsersItem/UsersItem.jsx
+++ src/components/MessagePage/UsersItem/UsersItem.jsx
@@ -6,7 +6,7 @@ const UsersItem = (props) => {
     return props.state.usersItem.map((item) =>
         <div className={s.app_users}>
             <img src="https://cdn-icons-png.flaticon.com/128/3237/3237472.png" alt="" />
-            <NavLink to={item.id}>{item.name}</NavLink>
+            <NavLink to={item.id} className={navData => navData.isActive ? s.active : s.item}>{item.name}</NavLink>
         </div>
     )
 };
